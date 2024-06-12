2b25535c2d63b6d64c05280e1bb660f668bbd69c
Shreya-sinha <sinhashreya2679@gmail.com>
2024-06-13 04:04:07 +0530

    fix: deletion bugs

--- src/components/todoitem.jsx
+++ src/components/todoitem.jsx
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types' 
 
-const Todoitem = ({todoName,todoDate}) => {
+const Todoitem = ({todoName,todoDate,onDeleteItem}) => {
   return (
     <div>
       <div className="row a-row">
         <div className="col-4 item">{todoName}</div>
         <div className="col-4 item">{todoDate}</div>
         <div className="col-4">
-          <button type="button" className="btn btn-dark shadow rounded-pill">
-            Remove
+          <button type="button" className="btn btn-dark shadow rounded-pill"
+          onClick={() => onDeleteItem(todoName)}>
+            Done
           </button>
         </div>
       </div>
@@ -18,5 +19,6 @@ const Todoitem = ({todoName,todoDate}) => {
 Todoitem.propTypes = {
   todoName: PropTypes.string,
   todoDate: PropTypes.string,
+  onDeleteItem: PropTypes.object,
 };
 export default Todoitem
